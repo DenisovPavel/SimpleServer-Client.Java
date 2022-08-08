a8624ba394353903732eeaa1bf64ea9d0444d702
TigerMommy <waste11@mail.ru>
2022-08-08 15:25:55 +0300

    second lesson

--- LevelOne/src/main/java/Lesson11/Student.java
+++ LevelOne/src/main/java/Lesson11/Student.java
@@ -1,11 +1,11 @@
 package Lesson11;
 
 public class Student {
-    String name;
-    int course;
-    double grade;
+    public String name;
+    public int course;
+   public double grade;
 
-    Student(String name, int course, double grade) {
+   public  Student(String name, int course, double grade) {
         this.name = name;
         this.course = course;
         this.grade = grade;
@@ -21,33 +21,34 @@ public class Student {
     public static void changeName(Student s3) {
         s3.name = "Vasiliy";
     }
-
-
-    public static void main(String[] args) {
-        Student st1 = new Student("Ivan", 3, 9.5);
-        Student st2 = new Student("Petr", 1, 5.3);
-
-        if (st1 == st2) {
-            System.out.println("Студенты ровны между собой!");
+    public static void toCampare (Student st1,Student st2){
+        if (st1.name.equals(st2.name) && st1.course==st2.course && st1.grade == st2.grade) {
+            System.out.println("Студенты ровны, курс и оценки совпадают по первому методу!");
         } else {
-            System.out.println("Студенты НЕ ровны  между собой!");
+            System.out.println("Студенты НЕ ровны  между собой по всем параметрам первого метода!");
         }
+    }
 
-if (st1.name==st2.name){
-    System.out.println("Имена студентов ровны между собой!");}
-    else if (st1.name!= st2.name)
-    System.out.println("Имена студентов не совпадают!");
+    public static void toCompare2(Student st1, Student st2){
+        if (st1.name == st2.name) {
+            if (st1.course == st2.course) {
+                if (st1.grade == st2.grade) {
+                    System.out.println("Имена студентов, курс и оценки  ровны между собой!*");
+                } else
+                    System.out.println("Имена студентов и Год(Курс) обучения совпадют, но Средняя оценка  не совпадает!*");
 
-    if (st1.course==st2.course){
-        System.out.println("Год(Курс) обучения студентов совпадает!");}
-    else if (st1.course!= st2.course)
-        System.out.println("Год(Курс) обучения студентов не совпадает!");
+            } else
+                System.out.println("Имена студентов ровны, но Год(Курс) обучения студентов не совпадает!*");
 
-    if(st1.grade== st2.grade){
-            System.out.println("Средняя оценка студентов совпадает!");}
-    else if (st1.grade!= st2.grade)
-        System.out.println("Средняя оценка студентов не совпадает!");
+        } else
+            System.out.println("Имена студентов, Год(Курс) обучения ,Средняя оценка  не совпадает!");
+    }
+    public static void main(String[] args) {
+        Student st1 = new Student("Ivan", 1, 9.5);
+        Student st2 = new Student("Ivan", 1, 5.3);
 
+        toCampare(st1, st2);
+        toCompare2(st1,st2);
 }
     }
 

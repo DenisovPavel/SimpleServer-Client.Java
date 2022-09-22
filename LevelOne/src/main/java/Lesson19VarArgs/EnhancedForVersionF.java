d319bb74cede9bd1aba3be4cbd7d69dd74f3e857
TigerMommy <waste11@mail.ru>
2022-09-22 11:44:56 +0300

    second lesson

---
+++ LevelOne/src/main/java/Lesson19VarArgs/EnhancedForVersionF.java
@@ -0,0 +1,16 @@
+package Lesson19VarArgs;
+
+public class EnhancedForVersionF {
+    public static void main(String[] args) {
+        int[] array = new int[4];
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = i * 10;
+//   }
+        for(int b:array){
+            b = 10; // с помощью foreach loop мы не можем проводить динамическую инициализацию массива;
+        }
+        for (int a : array) {
+            System.out.print(a + " ");// 0 10 20 30
+        }
+    }
+}

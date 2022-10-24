74f16c576ed5707a71011da03ecbea7d1661417a
TigerMommy <waste11@mail.ru>
2022-10-24 23:31:05 +0300

    gh

--- LevelOne/src/main/java/InheritanceLesson/KonstructorEx.java
+++ LevelOne/src/main/java/InheritanceLesson/KonstructorEx.java
@@ -1,29 +1,35 @@
 package InheritanceLesson;
+
 // overloaded constructors;
 public class KonstructorEx {
-//    KonstructorEx(String n, String s) {
-//        this.name = n;
-//        this.surname = s;
-//    }
-//        KonstructorEx(String n) {
-//            this(n, null);
-//        }
-//
-//    String name;
-//    String surname;
-//}
-//
+    KonstructorEx(String n, String s) {
+        this.name = n;
+        this.surname = s;
+    }
+
+    KonstructorEx(String n) {
+        this(n, null);// может обратиться к оверлоадед конструктору класса, который ниже;
+    }
 
     String name;
-  String surname;
+    String surname;
 }
 
-class Students extends KonstructorEx{
-    Students(){
-        super();// обращение к предку конструктору;
+class Students extends KonstructorEx {
+    //    Students() {
+//        super("Petya");// обращение к предку конструктору;
+//    }
+    Students() {
+        this(5);
+        System.out.println("Hello!");
     }
+
+    Students(int i) {
+       super("Petya");
+
+    }
+
     public static void main(String[] args) {
         Students s2 = new Students();
     }
-
 }
